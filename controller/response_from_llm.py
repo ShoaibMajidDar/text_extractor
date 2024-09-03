839fd7be9a967eaf6432217192dc2a392b9ca1eb
Shoaib Majid <126547607+ShoaibMajidDar@users.noreply.github.com>
2024-09-03 16:38:14 +0530

    bug fixes

--- controller/response_from_llm.py
+++ controller/response_from_llm.py
@@ -9,39 +9,9 @@ load_dotenv()
 api_key = os.getenv("api_key")
 
 aclient = openai.AsyncOpenAI(api_key=os.getenv("api_key"))
-# def get_response_from_llm(pdf_text, model, prompt):
-
-#     llm = ChatOpenAI(
-#     temperature=0, model=model, api_key=api_key
-#     )
-#     template = """
-    # Instructions:
-    # Task: Extract specific details from the provided text based on the user's query.
-    # Answer strictly according to the user's question.
-    # Do not infer or fabricate information. If the requested detail is not found in the text, state that it is not present.
-    # Adhere to any specific format the user requests for the response.
-# ==============================================
-#     User Query: {prompt}
-# ==============================================
-#     Source Text: {pdf_text}
-#     """
-#     prompt = ChatPromptTemplate.from_template(template)
-
-#     chain = prompt | llm | StrOutputParser()
-
-#     res = chain.invoke({"prompt": prompt, "pdf_text": pdf_text})
-#     print(template)
-#     return res
-
 
 async def get_response_from_llm(pdf_text, model, prompt):
     
-    # template = """Instructions:
-    # Task: Extract specific details from the provided text based on the user's query.
-    # Answer strictly according to the user's question.
-    # Do not infer or fabricate information. If the requested detail is not found in the text, state that it is not present.
-    # Adhere to any specific format the user requests for the response."""
-
     messages = [
         {"role": "system", "content": prompt},
         {"role": "user", "content": pdf_text}
